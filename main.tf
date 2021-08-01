a42cc73519ac242d81f3083f768bb523532979c2
subho-bm18 <41873066+subho-bm18@users.noreply.github.com>
2021-08-01 22:05:47 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,7 +9,6 @@ provider "aws" {
 resource "aws_instance" "ubuntu" {
   ami           = "${var.ami_id}"
   instance_type = "${var.instance_type}"
-  availability_zone = "${var.aws_region}a"
 
   tags ={
     Name = "${var.name}"
